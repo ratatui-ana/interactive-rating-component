08d8be64d6db594e21317f8ea80922385418e679
Ana Cristina <anacris0108@hotmail.com>
2023-01-25 19:27:32 -0300

    melhorias no estilo

---
+++ main.js
@@ -0,0 +1,6 @@
+bt1 = document.getElementById("bt1");
+bt2 = document.getElementById("bt2");
+bt3 = document.getElementById("bt3");
+bt4 = document.getElementById("bt4");
+bt5 = document.getElementById("bt5");
+
